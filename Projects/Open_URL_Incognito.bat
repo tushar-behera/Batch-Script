f377282b7c1b91117bebeb27bbbf30f812cea380
Tushar <tusharbehera32@gmail.com>
2023-11-21 12:12:03 +0530

    Task 2 cmplt

--- Projects/Open_URL_Incognito.bat
+++ Projects/Open_URL_Incognito.bat
@@ -3,3 +3,4 @@ Title Open_Incognito_Url
 echo This batch script is to open url in Incognito mode
 pause
 start /d "C:\Program Files\Google\Chrome\Application" Chrome.exe -incognito www.google.com
+start msedge.exe -inprivate www.google.com
