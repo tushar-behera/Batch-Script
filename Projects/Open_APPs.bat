c9c7a627af367f62630cb5e1503c386131b68793
Tushar <tusharbehera32@gmail.com>
2023-11-21 13:39:20 +0530

    Task 3 & 4 completed

---
+++ Projects/Open_APPs.bat
@@ -0,0 +1,6 @@
+@echo off
+Title Batch App-Opener
+echo This script is used to open prefer Apps
+pause
+start notepad.exe
+start WINWORD.EXE
---
+++ Projects/Open_file.bat
@@ -0,0 +1,6 @@
+@echo off
+Title File Oppener
+echo Used to open a specific Files
+pause
+start WINWORD.EXE "D:\For Illustration.docx"
+exit
