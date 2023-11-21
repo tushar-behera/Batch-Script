102b127d9488d18e9aeafd977497ea73b685e5e8
Tushar <tusharbehera32@gmail.com>
2023-11-21 12:05:17 +0530

    initial commit

---
+++ Projects/Open_URL.bat
@@ -0,0 +1,6 @@
+@echo off
+Title Open_URL Batch Script
+echo This script is to open daily url
+pause
+start www.google.com
+start msedge.exe https://www.linkedin.com/in/tusharbehera/
---
+++ Projects/Open_URL_Incognito.bat
@@ -0,0 +1,5 @@
+@echo off
+Title Open_Incognito_Url
+echo This batch script is to open url in Incognito mode
+pause
+start /d "C:\Program Files\Google\Chrome\Application" Chrome.exe -incognito www.google.com
